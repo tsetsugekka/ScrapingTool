3f2780f41b094184a26c3431404fa9e76aa7d4b2
tóng <kanagawa00@gmail.com>
2021-07-09 09:08:16 +0900

    Add files via upload

--- 20210709_ScrapingScript_GreenJapan.py
+++ 20210709_ScrapingScript_GreenJapan.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
+# In[8]:
 
 
 #制作requirements.txt
 #!pipreqsnb 20210709_ScrapingScript_GreenJapan.ipynb --encoding=utf8 --force --savepath requirements.txt
 
 #打包exe
-#pyinstaller ./20210709_ScrapingScript_GreenJapan.py -D --clean --noconsole -n "ScrapingTool" -i fav.ico --add-binary "./driver/chromedriver.exe;./driver" --add-binary "./browser;./browser"
+#pyinstaller ./20210709_ScrapingScript_GreenJapan.py --onefile --clean --noconsole -n "ScrapingTool" -i fav.ico --add-binary "./driver/chromedriver.exe;./driver" --add-binary "./browser;./browser"
+
+#（显示控制台）
+#pyinstaller ./20210709_ScrapingScript_GreenJapan.py -D --clean -n "ScrapingTool" -i fav.ico --add-binary "./driver/chromedriver.exe;./driver" --add-binary "./browser;./browser"
 
 
-# In[5]:
+#已知的问题
+#1）resource_path在jupyter执行报错，在jupyter执行脚本时需要不启用resource_path。
+#2）打包exe后chromium无法被调用。这里用了以文件夹形式打包（参数-D），打包后手动将chromium复制到打包后文件夹的方式解决。
+#3）提示缺少openpyxls库，按以下方法解决：https://blog.csdn.net/weixin_30907523/article/details/102154787
+
+
+# In[50]:
 
 
 #GUI
@@ -38,6 +47,44 @@ import chromedriver_binary
 #https://www.zacoding.com/post/python-selenium-pyinstaller/
 #PyInstaller の--add-binaryオプションを使用してブラウザとドライバも一緒にバンドルします。
 
+#随机等待时间
+import random
+def timesleep(max_num=4):
+    time.sleep(random.randint(2,max_num))
+    
+#替换文件名中不合法字符
+import re
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "_", title)  # 替换为下划线
+    return new_title
+
+#Pandas输出Excel自适应调整宽高
+#https://cloud.tencent.com/developer/article/1770494
+#https://www.jianshu.com/p/a3aed25b3c28
+from openpyxl.utils import get_column_letter
+from pandas import ExcelWriter
+import numpy as np
+
+def to_excel_auto_column_weight(df: pd.DataFrame, writer: ExcelWriter, sheet_name):
+    """DataFrame保存为excel并自动设置列宽"""
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    #  计算表头的字符宽度
+    column_widths = (
+        df.columns.to_series().apply(lambda x: len(x.encode('utf-8'))).values/3*2
+    )
+    #  计算每列的最大字符宽度
+    max_widths = (
+        df.astype(str).applymap(lambda x: len(x.encode('utf-8'))).agg(max).values/3*2
+    )
+    # 计算整体最大宽度
+    widths = np.max([column_widths, max_widths], axis=0)
+    # 设置列宽
+    worksheet = writer.sheets[sheet_name]
+    for i, width in enumerate(widths, 1):
+        # openpyxl引擎设置字符宽度时会缩水0.5左右个字符，所以干脆+2使左右都空出一个字宽。
+        worksheet.column_dimensions[get_column_letter(i)].width = width + 2
+
 #pyinstaller用 相对路径变换绝对路径
 def resource_path(relative_path):
     try:
@@ -48,18 +95,18 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-# In[3]:
+# In[13]:
 
 
 #ソース内でブラウザとドライバのパスを指定する
 
 #pyinstaller用
-browser_path = resource_path('browser/chrome.exe')
-driver_path = resource_path('driver/chromedriver.exe')
+#browser_path = resource_path('browser/chrome.exe')
+#driver_path = resource_path('driver/chromedriver.exe')
 
 #jupyter用
-#browser_path='browser/chrome.exe'
-#driver_path='driver/chromedriver.exe'
+browser_path='browser/chrome.exe'
+driver_path='driver/chromedriver.exe'
 
 def greenjapan(keyword):
 
@@ -108,16 +155,41 @@ def greenjapan(keyword):
         smallbuz_list=browser.find_elements_by_xpath('//*[@class="card-info__detail-area__box__body"]/ul[2]/li[4]') #小業界
 
         hreflist=browser.find_elements_by_xpath('//*[@class="js-search-result-box card-info "]')
-        time.sleep(1)
+        timesleep()
 
         for cn,yr,em,ag,bz,sb,hf in zip(company_name_list,year_list,employee_list,age_list,buz_list,smallbuz_list,hreflist):
             name=cn.text
-            year=yr.text
-            year=year.replace("設立年月日 ","")
-            employee=em.text
+            
+            ul1=yr.text
+            ul2=em.text
+            ul3=ag.text
+            
+            if "設立年月日" in ul1:
+                year=ul1.replace("設立年月日 ","")
+                if "従業員数" in ul2:
+                    employee=ul2.replace("従業員数 ","").replace("人","")
+                    if "平均年齢" in ul3:
+                        age=ul3.replace("平均年齢","").replace("歳","")
+                    else:
+                        age=""
+                else:
+                    employee,age="",""
+            elif "従業員数" in ul1:
+                employee=ul1.replace("従業員数 ","").replace("人","")
+                year=""
+                if "平均年齢" in ul2:
+                    age=ul2.replace("平均年齢","").replace("歳","")
+                else:
+                    age=""
+            elif "平均年齢" in ul1:
+                age=ul1.replace("平均年齢","").replace("歳","")
+                year,emplyee="",""
+            else:
+                year,emplyee,age="","",""
+                     
             employee=employee.replace("従業員数 ","").replace("人","")
-            age=ag.text
             age=age.replace("平均年齢","").replace("歳","")
+            
             buz=bz.text
             sbuz=sb.text
             href=hf.get_attribute("href")
@@ -163,7 +235,7 @@ def greenjapan(keyword):
         jobcontent=browser.find_element_by_xpath('//*[@class="com_content__basic-info"]').text #获得职业介绍页全部正文
 
         browser.find_element_by_id("com_menu_com_detail").click()
-        time.sleep(1)
+        timesleep()
         companycontent=browser.find_element_by_xpath('//article[@class="paragraphs"]').text #获得公司介绍页全部正文
 
         jobtext=searchresult(jobcontent)
@@ -173,7 +245,7 @@ def greenjapan(keyword):
         data[n].append(jobtext)
         data[n].append(comtext)
 
-        time.sleep(1)
+        timesleep()
 
     browser.close()
 
@@ -184,7 +256,7 @@ def greenjapan(keyword):
     return df
 
 
-# In[6]:
+# In[12]:
 
 
 #Tkinter GUI
@@ -228,6 +300,7 @@ searchsite=ttk.Combobox(window,values=["Green Japan",
                                        "PR Times（Not Available）"],
                         textvariable=var_site,
                         state="readonly") #readonly -文本字段不可编辑，用户只能从下拉列表中选择值。
+searchsite.current(0) #默认为0位
 searchsite.place(x=300, y=200, height=25, width=380)
 
 # Entry输入框，输入的值必须要定义
@@ -237,21 +310,25 @@ var_keywrod.place(x=300, y=250, height=25, width=380)
 def get_tar():
     site = var_site.get()
     window_keyword = var_keywrod.get()
+    filename="検索結果（キーワード："+validateTitle(window_keyword)+" データベース：GreenJapan）"
     
     if var_path.get()=="":
         #pyinstaller用
-        window_savepath=resource_path("pandas_to_excel.xlsx")
+        #window_savepath=resource_path("pandas_to_excel.xlsx")
         #jupyter用
-        #window_savepath="pandas_to_excel.xlsx"
+        window_savepath=filename+".xlsx"
     else:
-        window_savepath = var_path.get()+"/pandas_to_excel.xlsx"
+        window_savepath = var_path.get()+"/"+filename+".xlsx"
         
     if window_keyword == "":
         show_text.insert(END,"検索キーワードを入力してください。\n")
     else:
         if site == "Green Japan":
             doc=greenjapan(window_keyword)
-            doc.to_excel(window_savepath, index=False)
+            #doc.to_excel(window_savepath, index=False)
+            with pd.ExcelWriter(window_savepath, engine='openpyxl') as writer:
+                to_excel_auto_column_weight(doc, writer, "結果")
+            
             show_text.insert(END,
                              "（サイト："+site+"、キーワード："+window_keyword+"）でのスクレピング結果を"+window_savepath+"に保存されました。\n")
         else:
